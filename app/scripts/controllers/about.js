7fc506ede3fc5092e34cadfb7ff27c4b7881d4cf
Bruno Alves <BrunoCau@MacBook-Pro-de-Bruno.local>
2015-08-01 11:22:36 -0300

    first commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name projetoFortesApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the projetoFortesApp
+ */
+angular.module('projetoFortesApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name projetoFortesApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the projetoFortesApp
+ */
+angular.module('projetoFortesApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
